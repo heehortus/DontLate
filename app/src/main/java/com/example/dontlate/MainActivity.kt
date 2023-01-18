2102d6ec9610ed0de64c399f856e39e988c47a97
HeeWon <hee.hortus@gmail.com>
2023-01-19 03:49:58 +0900

    #3 Updated MainActivity.kt layout

--- app/src/main/java/com/example/dontlate/MainActivity.kt
+++ app/src/main/java/com/example/dontlate/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.dontlate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var splashScreen: SplashScreen
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        splashScreen = installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
